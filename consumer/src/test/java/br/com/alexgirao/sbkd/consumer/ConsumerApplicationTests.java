2edc9a6f06d5845a71bead3d35aca4c16c9667d6
Alex Girao <fco.alex.girao@gmail.com>
2021-11-30 23:49:46 -0300

    Iniciando Projeto

---
+++ consumer/src/test/java/br/com/alexgirao/sbkd/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alexgirao.sbkd.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
